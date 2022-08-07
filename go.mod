0ad716ebac988c151eacb1527fc952a5c22dbbf9
steden <1470804@qq.com>
2022-08-07 19:38:31 +0800

    迁移子库

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/farseer-go/mapper
+
+go 1.19
+
+require (
+	github.com/devfeel/mapper v0.7.10
+	github.com/farseer-go/fs v0.0.0-20220807091751-56d67e061446
+)
---
+++ mapper.go
@@ -0,0 +1,26 @@
+package mapper
+
+import (
+	"github.com/devfeel/mapper"
+	"github.com/farseer-go/fs/core"
+)
+
+// Array 数组转换
+func Array[T any](fromSlice any) []T {
+	var toSlice []T
+	_ = mapper.MapperSlice(fromSlice, &toSlice)
+	return toSlice
+}
+
+// Single 单个转换
+func Single[T any](fromObj any) T {
+	var toObj T
+	_ = mapper.MapperSlice(fromObj, &toObj)
+	return toObj
+}
+
+// PageList 转换成core.PageList
+func PageList[TData any](fromObj any, recordCount int64) core.PageList[TData] {
+	lst := Array[TData](fromObj)
+	return core.NewPageList(lst, recordCount)
+}
