0bc3a5fea9392e8e3f8926ebba52c7d82221c274
steden <1470804@qq.com>
2022-08-19 15:50:28 +0800

    修改单元测试为assert框架

--- go.mod
+++ go.mod
@@ -5,4 +5,11 @@ go 1.19
 require (
 	github.com/devfeel/mapper v0.7.10
 	github.com/farseer-go/collections v0.0.0-20220808152306-40c82a16a4bb
+	github.com/stretchr/testify v1.8.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- mapper_test.go
+++ mapper_test.go
@@ -2,6 +2,7 @@ package mapper
 
 import (
 	"github.com/farseer-go/collections"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -17,39 +18,32 @@ type do struct {
 func TestArray(t *testing.T) {
 	arrPO := []po{{Name: "steden", Age: 18}, {Name: "steden1", Age: 20}}
 	arrDO := Array[do](arrPO)
-	if len(arrPO) != len(arrDO) {
-		t.Error()
-	}
+	assert.Equal(t, len(arrPO), len(arrDO))
 
 	for i := 0; i < len(arrPO); i++ {
-		if arrPO[i].Name != arrDO[i].Name || arrPO[i].Age != arrDO[i].Age {
-			t.Error()
-		}
+		assert.Equal(t, arrPO[i].Name, arrDO[i].Name)
+		assert.Equal(t, arrPO[i].Age, arrDO[i].Age)
 	}
 }
 
 func TestSingle(t *testing.T) {
 	poSingle := po{Name: "steden", Age: 18}
 	doSingle := Single[do](&poSingle)
-	if poSingle.Name != doSingle.Name || poSingle.Age != doSingle.Age {
-		t.Error()
-	}
+
+	assert.Equal(t, poSingle.Name, doSingle.Name)
+	assert.Equal(t, poSingle.Age, doSingle.Age)
 }
 
 func TestPageList(t *testing.T) {
 	arrPO := []po{{Name: "steden", Age: 18}, {Name: "steden1", Age: 20}}
 	lst := PageList[do](arrPO, 10)
-	if len(arrPO) != lst.List.Count() {
-		t.Error()
-	}
 
-	if lst.RecordCount != 10 {
-		t.Error()
-	}
+	assert.Equal(t, len(arrPO), lst.List.Count())
+
+	assert.Equal(t, lst.RecordCount, int64(10))
 	for i := 0; i < len(arrPO); i++ {
-		if arrPO[i].Name != lst.List.Index(i).Name || arrPO[i].Age != lst.List.Index(i).Age {
-			t.Error()
-		}
+		assert.Equal(t, arrPO[i].Name, lst.List.Index(i).Name)
+		assert.Equal(t, arrPO[i].Age, lst.List.Index(i).Age)
 	}
 }
 
@@ -57,15 +51,13 @@ func TestToList(t *testing.T) {
 	lstAny := collections.NewListAny(po{Name: "steden", Age: 18}, po{Name: "steden1", Age: 20})
 	lstDO := ToList[do](lstAny)
 
-	if lstAny.Count() != lstDO.Count() {
-		t.Error()
-	}
+	assert.Equal(t, lstAny.Count(), lstDO.Count())
 
 	for i := 0; i < lstAny.Count(); i++ {
 		po := lstAny.Index(i).(po)
-		if po.Name != lstDO.Index(i).Name || po.Age != lstDO.Index(i).Age {
-			t.Error()
-		}
+
+		assert.Equal(t, po.Name, lstDO.Index(i).Name)
+		assert.Equal(t, po.Age, lstDO.Index(i).Age)
 	}
 }
 
@@ -73,25 +65,23 @@ func TestToListAny(t *testing.T) {
 	arrPO := []po{{Name: "steden", Age: 18}, {Name: "steden1", Age: 20}}
 
 	listAny := ToListAny(arrPO)
-	if listAny.Count() != len(arrPO) {
-		t.Error()
-	}
+
+	assert.Equal(t, listAny.Count(), len(arrPO))
 	for i := 0; i < listAny.Count(); i++ {
 		po := listAny.Index(i).(po)
-		if po.Name != arrPO[i].Name || po.Age != arrPO[i].Age {
-			t.Error()
-		}
+
+		assert.Equal(t, po.Name, arrPO[i].Name)
+		assert.Equal(t, po.Age, arrPO[i].Age)
 	}
 
 	lst := collections.NewList(arrPO...)
 	listAny = ToListAny(lst)
-	if listAny.Count() != len(arrPO) {
-		t.Error()
-	}
+
+	assert.Equal(t, listAny.Count(), len(arrPO))
 	for i := 0; i < listAny.Count(); i++ {
 		po := listAny.Index(i).(po)
-		if po.Name != arrPO[i].Name || po.Age != arrPO[i].Age {
-			t.Error()
-		}
+
+		assert.Equal(t, po.Name, arrPO[i].Name)
+		assert.Equal(t, po.Age, arrPO[i].Age)
 	}
 }
