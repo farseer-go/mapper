f19c816867d7539f8d99a4218f76f0eaab70f707
gelin9527 <1376609520@qq.com>
2022-12-22 14:29:54 +0800

    删除不使用代码

--- mapper.go
+++ mapper.go
@@ -1,7 +1,6 @@
 package mapper
 
 import (
-	"fmt"
 	"github.com/farseer-go/collections"
 	"reflect"
 	"strings"
@@ -20,99 +19,6 @@ func Array[T any](fromSlice any) []T {
 		toSlice = append(toSlice, tInfo)
 	}
 	return toSlice
-}
-
-func AutoMapper(fromDO, toDTO any) error {
-	fs := reflect.TypeOf(fromDO)
-	if fs.Kind() != reflect.Ptr {
-		return fmt.Errorf("fromDO must be a struct pointer")
-	}
-	ts := reflect.TypeOf(toDTO)
-	if ts.Kind() != reflect.Ptr {
-		return fmt.Errorf("toDTO must be a struct pointer")
-	}
-	objMap := structToMap(reflect.ValueOf(fromDO), "")
-
-	//fmt.Println(objMap)
-	tsVal := reflect.ValueOf(toDTO).Elem()
-	setStructVal(objMap, &tsVal, "")
-	return nil
-}
-
-// 设置值
-func setStructVal(objMap map[string]interface{}, tsVal *reflect.Value, pre string) {
-	for i := 0; i < tsVal.NumField(); i++ {
-		f := tsVal.Type().Field(i)
-		name := pre + f.Name
-		cv := tsVal.Field(i)
-		var objVal any
-		objVal = objMap[name]
-		//fmt.Println(name)
-		if objVal == nil && tsVal.Field(i).Kind() == reflect.Struct {
-			setStructVal(objMap, &cv, name)
-			continue
-		} else if objVal == nil {
-			continue
-		}
-		objType := reflect.TypeOf(objVal)
-		//fmt.Println(name, f.Type.Kind(), objType.Kind(), objVal)
-		if f.Type.Kind() == objType.Kind() {
-			tsVal.Field(i).Set(reflect.ValueOf(objVal))
-		} else if tsVal.Field(i).Kind() == reflect.Struct {
-			setStructVal(objMap, &cv, name)
-			continue
-		}
-	}
-}
-
-// struct转map
-func structToMap(obj reflect.Value, name string) map[string]any {
-	objMap := make(map[string]interface{})
-	switch obj.Kind() {
-	case reflect.Ptr:
-		if name != "" {
-			objMap[name] = structToMap(obj.Elem(), name)
-		} else {
-			objMap = structToMap(obj.Elem(), name)
-		}
-	case reflect.Struct:
-		for i := 0; i < obj.NumField(); i++ {
-			f := obj.Field(i)
-			prename := name
-			name := name + obj.Type().Field(i).Name
-			if f.Kind() == reflect.Struct || f.Kind() == reflect.Ptr {
-				cMap := structToMap(f, name)
-				objMap[name] = cMap
-				for mk, mv := range cMap {
-					objMap[mk] = mv
-				}
-			} else {
-				if f.CanInterface() {
-					objMap[name] = f.Interface()
-				} else if f.Kind() == reflect.Map {
-
-					newMaps := make(map[string]string)
-					maps := f.MapRange()
-					for maps.Next() {
-						str := fmt.Sprintf("%v=%v", maps.Key(), maps.Value())
-						array := strings.Split(str, "=")
-						newMaps[array[0]] = array[1]
-					}
-					dic := collections.NewDictionaryFromMap(newMaps)
-					objMap[prename] = dic
-					//m := make(map[any]any)
-					//iter := f.MapRange()
-					//for iter.Next() {
-					//	k := iter.Key()
-					//	v := iter.Value()
-					//	m[k] = v
-					//}
-					//objMap[name] = m
-				}
-			}
-		}
-	}
-	return objMap
 }
 
 // Single 单个转换
