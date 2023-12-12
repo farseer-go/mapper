71a4c086f22b157d64d589833665d6f043351693
steden <1470804@qq.com>
2023-12-12 14:22:38 +0800

    调整：ToPageList方法，移除recordCount入参

--- test/mapper_test.go
+++ test/mapper_test.go
@@ -67,7 +67,7 @@ func TestSingle(t *testing.T) {
 }
 
 func TestPageList(t *testing.T) {
-	arrDto := []TaskDTO{{
+	arrDto := collections.NewPageList[TaskDTO](collections.NewList(TaskDTO{
 		Id:         1,
 		ClientId:   1000,
 		ClientIp:   "127.0.0.1",
@@ -75,7 +75,7 @@ func TestPageList(t *testing.T) {
 		Status:     Pending,
 		User:       UserVO{Id: 88, Name: "steden"},
 		Data:       collections.NewDictionaryFromMap(map[string]string{"age": "18", "price": "88.88"}),
-	}, {
+	}, TaskDTO{
 		Id:         2,
 		ClientId:   1000,
 		ClientIp:   "127.0.0.1",
@@ -83,16 +83,16 @@ func TestPageList(t *testing.T) {
 		Status:     Pending,
 		User:       UserVO{Id: 20, Name: "steden1"},
 		Data:       collections.NewDictionaryFromMap(map[string]string{"age": "18", "price": "88.88"}),
-	}}
+	}), 10)
 
-	lst := mapper.ToPageList[TaskDO](arrDto, 10)
+	lst := mapper.ToPageList[TaskDO](arrDto)
 
-	assert.Equal(t, len(arrDto), lst.List.Count())
+	assert.Equal(t, arrDto.List.Count(), lst.List.Count())
 
 	assert.Equal(t, lst.RecordCount, int64(10))
-	for i := 0; i < len(arrDto); i++ {
-		assert.Equal(t, arrDto[i].User.Name, lst.List.Index(i).UserName)
-		assert.Equal(t, arrDto[i].User.Id, lst.List.Index(i).UserId)
+	for i := 0; i < arrDto.List.Count(); i++ {
+		assert.Equal(t, arrDto.List.Index(i).User.Name, lst.List.Index(i).UserName)
+		assert.Equal(t, arrDto.List.Index(i).User.Id, lst.List.Index(i).UserId)
 	}
 }
 
