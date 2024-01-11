f410332e38aaf511313ce5b7ce6dd72085445f45
steden <1470804@qq.com>
2024-01-11 22:27:11 +0800

    添加测试

--- test/mapToEntity_test.go
+++ test/mapToEntity_test.go
@@ -7,13 +7,15 @@ import (
 )
 
 type BaseEntity struct {
-	AppId   int64  // 应用ID
-	AppName string // 应用名称
+	Exception SubMapEntity
+	AppId     int64  // 应用ID
+	AppName   string // 应用名称
 }
 type SubMapEntity struct {
 	Age     int    // 应用ID
 	Caption string // 应用名称
 }
+
 type MapEntity struct {
 	BaseEntity
 	UserId   int
@@ -27,6 +29,10 @@ func TestMapToEntity(t *testing.T) {
 		"Age":     18,
 		"Caption": "有值吗",
 	}
+	m["Exception"] = map[string]any{
+		"Age":     22,
+		"Caption": "嵌入字段",
+	}
 	m["AppId"] = int64(1)
 	m["AppName"] = "test"
 	m["UserId"] = 888
@@ -39,4 +45,6 @@ func TestMapToEntity(t *testing.T) {
 	assert.Equal(t, m["UserName"], entity.UserName)
 	assert.Equal(t, m["Sub"].(map[string]any)["Age"], entity.Sub.Age)
 	assert.Equal(t, m["Sub"].(map[string]any)["Caption"], entity.Sub.Caption)
+	assert.Equal(t, m["Exception"].(map[string]any)["Age"], entity.Exception.Age)
+	assert.Equal(t, m["Exception"].(map[string]any)["Caption"], entity.Exception.Caption)
 }
